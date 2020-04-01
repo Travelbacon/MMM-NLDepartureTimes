170899462372494d1ccd557a8c745940d378f631
George Travelbacon <joris@ridder.lan>
2020-04-01 21:55:11 +0200

    Data received and parsed. Now styling.

--- MMM-NLDepartureTimes.js
+++ MMM-NLDepartureTimes.js
@@ -1,5 +1,10 @@
 Module.register("MMM-NLDepartureTimes", {
 
+	naam: "Joris",
+	
+	defaults: {
+		things: -1
+	},
 
 	start: function(){
 		Log.info(`Sarting module ${this.name}`);
@@ -7,8 +12,27 @@ Module.register("MMM-NLDepartureTimes", {
 	
 	getDom: function(){
 		let wrapper = document.createElement("div");
-		wrapper.innerHTML = "Joris";
-		return wrapper;
-	}
+		wrapper.innerHTML = this.naam;
+    this.sendSocketNotification('SET_CONFIG', this.config);
+		return wrapper;		
+	},
 	
+	socketNotificationReceived: function(notification, payload) {
+		Log.info('Helper send data.');
+    Log.info(notification);
+		this.printTimeTable(payload);
+  },
+
+  printTimeTable: function(timeTableList){
+    for(const stopArea in timeTableList){
+      console.log(`${stopArea}`);
+      for(const direction in timeTableList[stopArea]){
+        console.log(` ${direction}`);
+        for(const vehicle of timeTableList[stopArea][direction]){
+          console.log(new Date(vehicle.DepTime).toLocaleTimeString('nl-NL') + ' ( ' + vehicle.Delay + ' ) : ' + vehicle.LineName + ' - ' + vehicle.Destination);  
+        }
+      }
+    }
+  }
+  
 });
---
+++ node_helper.js
@@ -0,0 +1,88 @@
+//From configuration in the magic mirror.
+
+const axios = require('axios');
+
+let URLovapi;
+let URLtpc = '';
+let timeTableList = new Object();
+
+var NodeHelper = require("node_helper");
+module.exports = NodeHelper.create({
+
+  socketNotificationReceived: function(notification, payload) {
+    console.log('Helper received data.');
+    console.log(payload);
+    this.run(payload);
+  },
+
+    
+  parseData: function(jsonData, stopCodeConfig) {
+/* 
+  The JSON comes with level 1 with the time table for a stop code.
+  This code wants to group the times not on a stop level, but on a direction level.
+  A direction A can have multilple stops.
+  Best way is to look in the JSON per stop, and find the proper direction.
+*/
+  //JSON has on level 1 the stop codes.
+  //The time tables from different stops must be sorted to a direction.
+  console.log('Parsing.');
+  //Get the stopCode from the config and check if present in JSON.
+  //If present parse the node in the timetable.  
+  for(let confHaltGroup in stopCodeConfig){ //Find the haltgroup/area in the group
+  //Each direction has multipe physique halts with its own stopcode
+    for(let confDirection in stopCodeConfig[confHaltGroup]){ 
+      for(let confStopCode of stopCodeConfig[confHaltGroup][confDirection]){
+        if(confStopCode in jsonData){
+          //Make an object of the vehicle to add in timeTableList
+          for(let vehicInfo in jsonData[confStopCode]['Passes']){
+            let vehicRaw = jsonData[confStopCode]['Passes'][vehicInfo];
+            let vehicle = {
+              Journey: vehicRaw.JourneyNumber,
+              LineName: vehicRaw.LinePublicNumber,
+              TransportType: vehicRaw.TransportType,
+              DepTime: new Date(vehicRaw.ExpectedDepartureTime), //
+              DepTimeSched: new Date(vehicRaw.TargetDepartureTime),
+              Delay: new Date(vehicRaw.ExpectedDepartureTime).getMinutes() - new Date(vehicRaw.TargetDepartureTime).getMinutes(),
+              Destination: vehicRaw.DestinationName50
+            };
+            //Add the object in the list sorted on time.        
+            let i = 0;
+              for (; i < timeTableList[confHaltGroup][confDirection].length; i++) {
+                if (vehicle['DepTime'] <= timeTableList[confHaltGroup][confDirection][i]['DepTime']){
+                  break;
+                }
+              }
+              timeTableList[confHaltGroup][confDirection].splice(i,0,vehicle);
+            }
+          }
+        }
+      }
+    }
+    this.sendSocketNotification('TimeTable', timeTableList);
+  },
+
+  
+  run: function(stopCodeConfig){
+    var self = this;
+    
+    for(let haltGroup in stopCodeConfig){
+      timeTableList[haltGroup] = new Object;
+      for(let stopCode in stopCodeConfig[haltGroup]){
+        timeTableList[haltGroup][stopCode] = new Array();
+        URLtpc += stopCodeConfig[haltGroup][stopCode] + ',';
+      }
+    }
+
+    URLovapi = `http://v0.ovapi.nl/tpc/${URLtpc}/departures/`;
+
+    axios.get(URLovapi)
+    .then(function(response) {
+      //handle success
+      self.parseData(response.data, stopCodeConfig);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    });
+  }
+});
