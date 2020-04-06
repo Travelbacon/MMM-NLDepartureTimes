8b5567203974ce76f3a2751f896be578246b5b89
George Travelbacon <george@ridder.lan>
2020-04-06 22:04:35 +0200

    It works now.

--- MMM-NLDepartureTimes.js
+++ MMM-NLDepartureTimes.js
@@ -10,10 +10,9 @@ Module.register("MMM-NLDepartureTimes", {
     Log.info(`Sarting module ${this.name}`);
   },
 
-  
-    getStyles: function() {
-        return ["MMM-NLDepartureTimes.css"];
-    },
+  getStyles: function() {
+    return ["MMM-NLDepartureTimes.css"];
+  },
     
   getDom: function(){
     var self = this;
@@ -29,7 +28,8 @@ Module.register("MMM-NLDepartureTimes", {
         let row = document.createElement("tr");
         let lineHeader = document.createElement("th");
         lineHeader.innerHTML = stopArea;
-        lineHeader.colSpan = 2;
+        lineHeader.className = "bold";
+        lineHeader.colSpan = 3;
         row.appendChild(lineHeader);
         table.appendChild(row);
         //Fetch direction
@@ -37,7 +37,7 @@ Module.register("MMM-NLDepartureTimes", {
           let row = document.createElement("tr");
           let lineDirection = document.createElement("td");
           lineDirection.innerHTML = direction;
-          lineDirection.colSpan = 2;
+          lineDirection.colSpan = 3;
           lineDirection.className = "small";
           row.appendChild(lineDirection);
           table.appendChild(row);
@@ -49,14 +49,20 @@ Module.register("MMM-NLDepartureTimes", {
             let row = document.createElement("tr");
             let vehicleTime = document.createElement("td");
             vehicleTime.innerHTML = new Date(vehicle.DepTime).toLocaleTimeString('nl-NL');// + ' ' + vehicle.Delay;
-            vehicleTime.className = "xsmall vehicRow";
+            vehicleTime.className = "xsmall light vehicDepTime";
             row.appendChild(vehicleTime);
             
             //Create line number + destination
+            let vehicleLine = document.createElement("td");
+            vehicleLine.innerHTML = vehicle.LineName;
+            vehicleLine.className = "xsmall light vehicLine";
+            row.appendChild(vehicleLine);
+            
             let vehicleDestination = document.createElement("td");
-            vehicleDestination.innerHTML = vehicle.LineName + ' ' + vehicle.Destination;
-            vehicleDestination.className = "xsmall vehicRow";
+            vehicleDestination.innerHTML = vehicle.Destination;
+            vehicleDestination.className = "xsmall light vehicleDestination";
             row.appendChild(vehicleDestination);
+            
             table.appendChild(row);
             console.log(`Max :${this.config.maxVehics}`);
             if(vehicCounter === this.config.maxVehics){
